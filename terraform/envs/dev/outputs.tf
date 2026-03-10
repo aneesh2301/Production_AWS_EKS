4c1c79d5237a9413aa77abf04784aefef4b0214a
aneesh <aneeshyadav36@gmail.com>
2026-03-10 14:07:39 +0000

    cluster details required as outputs

--- terraform/envs/dev/outputs.tf
+++ terraform/envs/dev/outputs.tf
@@ -7,3 +7,15 @@ output "oidc_provider" {
   description = "The URL of the OIDC Provider"
   value       = module.eks.oidc_provider
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "kubeconfig_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
